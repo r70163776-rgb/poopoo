416e556db6490e958cd8c0bb214d77f6dd5cadc1
r70163776-rgb <r70163776@gmail.com>
2025-10-08 13:22:25 +0000

    Delete deepseek_javascript_20251008_046421.js

--- deepseek_javascript_20251008_046421.js
+++
@@ -1,9 +0,0 @@
-// Save to Telegram cloud
-Telegram.WebApp.CloudStorage.setItem('gameSave', JSON.stringify(gameState));
-
-// Load from Telegram cloud
-Telegram.WebApp.CloudStorage.getItem('gameSave', (error, value) => {
-    if (!error && value) {
-        gameState = JSON.parse(value);
-    }
-});
